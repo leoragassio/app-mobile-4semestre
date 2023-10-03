dc1d74a3458ae748e8a0662e1dc76e66a77389c6
adrielmuniz98 <107221529+adrielmuniz98@users.noreply.github.com>
2023-10-02 22:03:20 -0300

    Adriel 01

--- app1/lib/main.dart
+++ app1/lib/main.dart
@@ -3,20 +3,51 @@ import 'package:flutter/material.dart';
 void main() => runApp(Cores());
 
 class Cores extends StatelessWidget {
-  const Cores({super.key});
+
+ final Color corPrincipal = Colors.red;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Cores',
-      home: Scaffold(
+      home: new Scaffold(
         appBar: AppBar(
           title: Text('Cores'),
-          backgroundColor: Colors.red[200],
+          backgroundColor: corPrincipal,
           centerTitle: true,
         ),
-      ),
-    );
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Vermelho',
+                  style: TextStyle(
+                    fontSize: 50,
+                    color: corPrincipal,
+                  ),
+                ),         
+                Padding(
+                  padding: EdgeInsets.all(10),
+                ),
+                ElevatedButton(
+                  onPressed: (){}, 
+                  child: Text(
+                    'Trocar cor',
+                    style:  TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
   }
 }
 //Parei no video 8  da PlayList
