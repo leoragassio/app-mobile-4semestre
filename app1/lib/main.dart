65b380a923a7f10d0beb58ff353fbd332ac571c2
DantePedeTruco <leandro.oli9080@gmail.com>
2023-10-16 21:34:40 -0300

    Revert "video 10"
    This reverts commit 6bfdc1b646291de82de883aefda7b1e19a568a60.

--- app1/lib/main.dart
+++ app1/lib/main.dart
@@ -20,52 +20,14 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-List<Color> opcoesCorBarra;
-List<Color> opcoesCorTexto;
-List<Color> opcoesCorFundo;
-List<String> cores;
-int opcaoAtual;
-
-@override
-  void initState() {
-    super.initState();
-    opcoesCorBarra = [
-      Colors.green[900],
-      Colors.red[900],
-      Colors.blue[900],
-      Colors.orange[900],
-      Colors.purple[900],
-    ];
-
-    opcoesCorTexto = [
-      Colors.green[800],
-      Colors.red[800],
-      Colors.blue[800],
-      Colors.orange[800],
-      Colors.purple[800],
-
-    ];
-
-    opcoesCorFundo = [
-      Colors.green[50],
-      Colors.red[50],
-      Colors.blue[50],
-      Colors.orange[50],
-      Colors.purple[50],
-    ];
-
-    cores = ['Verde','Vermelho','Azul','Laranja','Roxo'];
-
-    opcaoAtual = 0;
-  }
+Color corPrincipal = Colors.red;
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      backgroundColor: opcoesCorFundo[opcaoAtual],
         appBar: AppBar(
           title: Text('Cores'),
-          backgroundColor: opcoesCorBarra[opcaoAtual],
+          backgroundColor: corPrincipal,
           centerTitle: true,
         ),
           body: Center(
@@ -74,10 +36,10 @@ int opcaoAtual;
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Text(
-                  cores[opcaoAtual],
+                  'Vermelho',
                   style: TextStyle(
                     fontSize: 50,
-                    color: opcoesCorTexto[opcaoAtual],
+                    color: corPrincipal,
                   ),
                 ),         
                 Padding(
@@ -86,8 +48,12 @@ int opcaoAtual;
                 ElevatedButton(
                   onPressed: (){
                     setState(() {
-                  opcaoAtual = 
-                  opcaoAtual < opcoesCorBarra.length - 1 ? ++opcaoAtual : 0;    
+                   
+                    if(corPrincipal == Colors.red){
+                      corPrincipal = Colors.blue;
+                    }else{
+                      corPrincipal = Colors.red;
+                    }
                     });
                   }, 
                   child: Text(
